85538d5146537a5fe32d865d9ab17e7950aea449
chenskiro <chenskiro@outlook.com>
2024-12-22 11:16:50 +0800

    update:(vite.config.js) 修复链接问题

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/', // 部署路径，根据需要修改
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  build: {
+    outDir: 'dist', // 打包输出目录
+    assetsDir: 'assets', // 静态资源目录
+  },
+  server: {
+    historyApiFallback: true, // 支持 History 模式
   },
 })
