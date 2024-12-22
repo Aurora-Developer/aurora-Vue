10b068eaaf591c02ad38062ad31951819c78f715
chenskiro <chenskiro@outlook.com>
2024-12-22 11:27:02 +0800

    update:(main.js) 修复链接问题

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 // import './assets/main.css'
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
-import Home from './components/Home.vue'
-import Download from './components/Download.vue'
-import './assets/styles/global.css' // 引入全局 CSS 文件
-import './assets/styles/common.css' // 引入 common CSS 文件
+import App from '@/App.vue'
+import Home from '@/components/Home.vue'
+import Download from '@/components/Download.vue'
+import '@/assets/styles/global.css' // 引入全局 CSS 文件
+import '@/assets/styles/common.css' // 引入 common CSS 文件
 
 // 创建路由实例
 const router = createRouter({
